71368d8641d7cbc6cf52e8f2bc5fc8b3525116a5
Mellafesa <mellafesarofida@gmail.com>
2022-01-27 08:33:22 +0700

    upload modul 2

--- routes/web.php
+++ routes/web.php
@@ -34,4 +34,6 @@ Route::get('/gallery', function (){
         "title" => "Gallery"
         ]);
 });
+use App\Http\Controllers\ContactController;
+Route::resource('/contacts', ContactController::class);
 
